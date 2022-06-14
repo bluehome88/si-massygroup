05de2d38f9c0926fee67f554accf7da86a22d52e
Blue Webmaster <bluehome88@outlook.com>
2022-06-14 12:14:25 -0700

    theme changes for Stock Price page

--- wp-content/themes/massy_group/sidebar-stock-data-section.php
+++ wp-content/themes/massy_group/sidebar-stock-data-section.php
@@ -1,4 +1,4 @@
-<div class="main_content_inner" style="margin-bottom: 58px;">
+<div class="main_content_inner" style="margin-bottom: 20px;">
   <div class="justify-content-between align-items-start w-100 chartTopInner">
     <?php dynamic_sidebar( 'stock-data-section' ); ?>
   </div>
